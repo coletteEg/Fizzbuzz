0c58962d470755e717279969a90018ada1b84e0a
colette <colette.believe@gmail.com>
2022-07-14 08:09:21 -0600

    initial commit

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+// loop 50
+// / by three fizz
+// / by five buzz
+// / by both 3 and 5 fizzbuzz
+// Write your solution below this line:
+
+for(let i = 1; i <= 50; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("Fizzbuzz")
+    } else if(i % 5 === 0){
+        console.log("Buzz")
+    } else if(i % 3 === 0){
+        console.log("Fizz")
+    } else{
+        console.log(i)}
+  }
